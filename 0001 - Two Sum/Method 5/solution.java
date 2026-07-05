8df25c1e0d2573c2a920176e83364d87a991c0f5
Mohd Rahim Ali <134539026+mrahimali@users.noreply.github.com>
2026-07-05 22:15:30 +0530

    Add Solution for Two Sum (Method 5)

---
+++ 0001 - Two Sum/Method 5/solution.java
@@ -0,0 +1,23 @@
+        :rtype: int
+        """
+        rev =0
+        while y>=0:
+            rem = y%10
+            rev = rev*10+rem
+        max = 2**31 - 1
+        min = -2**31 
+            if rev<min or rev>max:
+                return 0
+            
+        
+        if y<0:
+            y *=-1
+        y = x
+        if x<0:
+            rev *=-1
+        return rev
+        :type x: int
+        """
+    def reverse(self, x):
+class Solution(object):
+            y=y//10
